4ee99449c62f9517bdce436f02faff72dc446d54
Stuart Mumford <stuart@cadair.com>
2017-07-02 21:25:54 +0100

    SunPy Config

--- {{ cookiecutter.package_name }}/docs/conf.py
+++ {{ cookiecutter.package_name }}/docs/conf.py
@@ -124,6 +124,14 @@ html_theme_options = {
 
 {% endif %}
 
+{% if cookiecutter.sphinx_theme == "sphinx_rtd_theme" %}
+try:
+    import sphinx_rtd_theme
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except ImportError:
+    html_theme = 'default'
+{% endif %}
+
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
 
@@ -134,7 +142,7 @@ html_theme_options = {
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = ''
+html_favicon = ''
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
